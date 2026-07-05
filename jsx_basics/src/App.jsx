1d6a16493999c7f8abde215bacc1681232b4a412
dhrubmahajan17 <dhrubmahajan17@gmail.com>
2026-07-05 07:19:03 +0530

    jsx basic

---
+++ jsx_basics/src/App.jsx
@@ -0,0 +1,13 @@
+const App = () => {
+  return (
+    <section>
+      <h1>My Website</h1>
+      <article>
+        <h2>huhh SHIT </h2>
+        <p className="text">Hello , here we go again</p>
+      </article>
+    </section>
+  )
+}
+
+export default App
